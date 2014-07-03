660550765134b113817ccf86f9f3da86265f68bd
jaake <jmargason758@gmail.com>
2014-07-03 10:58:22 -0500

    create database tables

---
+++ lib/report_handler.rb
@@ -0,0 +1,42 @@
+
+module ReportHandler
+
+  require 'zip'
+  require 'libxml'
+
+
+  def self.unzip
+    zipfiles = []
+    uploads = Dir.entries("from_android/")
+    
+    uploads.each do |file|
+      zipfiles << file if /.zip/.match file
+    end
+    
+    zipfiles.each do |zipfile|
+      Zip::File.open("from_android/#{zipfile}") do |file|
+        file.each do |entry|
+          puts "Extracting #{entry.name}"
+          s_entry = entry.to_s
+          entry.extract "from_android/photos/#{entry.name}" if /jpg/.match s_entry
+          entry.extract "from_android/reports/#{Time.current.hash}#{entry.name}" if /xml/.match s_entry
+        end
+      end
+    end
+  end
+
+  def self.separate
+    
+    reports = Dir.entries(from_android/reports)  
+    reports.each do |report|
+      parser = LibXML::XML::Parser.file("from_android/reports/#{report}")
+      document = parser.parse
+      document.find("Report").each do |report|
+        
+
+        puts report
+      end
+    end
+  end
+
+end
